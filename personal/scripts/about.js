56e2f008f3767e2cf85ae8fe2de55144a91cfe89
justinbabe019 <justinbabe019@gmail.com>
2023-01-08 14:26:57 -0500

    now all the pictures and captions are on there, try to make them the same size

--- personal/scripts/about.js
+++ personal/scripts/about.js
@@ -19,12 +19,10 @@ function showSlides(n) {
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-    alert("ok!");
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  /*slides[slideIndex-1].style.display = "block";*/
-  slides[1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
