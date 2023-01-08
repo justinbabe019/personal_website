64cf420d814cf647437f87603973b8dc36101391
justinbabe019 <justinbabe019@gmail.com>
2023-01-08 14:16:17 -0500

    fixed slideshow issue with changing script location to bottom of body

--- personal/scripts/about.js
+++ personal/scripts/about.js
@@ -1,4 +1,3 @@
-alert("Hello");
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -20,10 +19,12 @@ function showSlides(n) {
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    alert("ok!");
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  /*slides[slideIndex-1].style.display = "block";*/
+  slides[1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
